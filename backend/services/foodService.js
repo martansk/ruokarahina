414c1b02502b60a3c919dbcbe383d70778395a99
Marjo Tanska <36375022+martansk@users.noreply.github.com>
2022-10-15 10:54:32 +0300

    foodService käyttämään axios-kirjastoa

--- backend/services/foodService.js
+++ backend/services/foodService.js
@@ -1,22 +1,17 @@
-const request = require('request');
-//const axios = require('axios');
+const axios = require('axios');
 // ruoan nimellä hakeminen tapahtuu esim. https://fineli.fi/fineli/api/v1/foods?q=omena
 const BASE_URL = 'https://fineli.fi/fineli/api/v1/foods?q='
 // ruoan id:llä hakeminen tapahtuu esim. https://fineli.fi/fineli/api/v1/foods/11060
 
+const searchFood = async (foodName) => {
+    try {
+        const data = await axios.get(BASE_URL + foodName);
+        return data.data;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
 module.exports = {
-    searchFood: (foodName) => {
-        return new Promise((resolve, reject) => {
-            let url = BASE_URL + foodName;
-            request.get(url, (error, response, body) => {
-                if(error) {
-                    reject(error);
-                } else if(response && response.statusCode === 200) {
-                    resolve(body);
-                } else {
-                    reject (body);
-                }
-            })
-        })
-  }
+    searchFood
 };
