b2aa8cf9e5200bd57d2b6b5840b7cbcd948567db
Marjo Tanska <36375022+martansk@users.noreply.github.com>
2022-10-25 18:36:35 +0300

    updated app.js
    - fixed body-parser warning

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ const express = require('express');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cors());
 app.use('/api/', routes);
 app.use(express.static('build'))
